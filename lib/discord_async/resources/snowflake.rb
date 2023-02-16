96e17ec43b7c8e3bb8ec7ef1029d5758514dcefc
Peter Runich <p.runich@bk.ru>
2023-02-16 16:34:19 +0300

    move snowflake to resources and inherited from StructBase

--- lib/discord_async/resources/snowflake.rb
+++ lib/discord_async/resources/snowflake.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
 module DiscordAsync
-  class Snowflake
-    def initialize(value)
-      @value = Types::Coercible::Integer[value]
-    end
+  module Resources
+    class Snowflake < StructBase
+      attribute :id, Types::Coercible::Integer
 
-    def internal_worker_id
-      (@value & 0x3E0000) >> 17
-    end
+      def internal_worker_id
+        (id & 0x3E0000) >> 17
+      end
 
-    def internal_process_id
-      (@value & 0x1F000) >> 12
-    end
+      def internal_process_id
+        (id & 0x1F000) >> 12
+      end
 
-    def increment
-      @value & 0xFFF
-    end
+      def increment
+        id & 0xFFF
+      end
 
-    def to_s
-      @value.to_s
-    end
+      def to_s
+        id.to_s
+      end
 
-    def to_time
-      Time.at ((@value >> 22) + 1_420_070_400_000) / 1000.0
+      def to_time
+        Time.at ((id >> 22) + 1_420_070_400_000) / 1000.0
+      end
     end
-  end
-
-  module Types
-    Snowflake = Types.Constructor(Snowflake)
   end
 end
