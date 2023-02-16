b1ced6eb6fd864b0a95406b077fefc1e78f285fa
Peter Runich <p.runich@bk.ru>
2023-02-16 16:36:11 +0300

    tmp commented

--- lib/discord_async/resources/application/resolved_data.rb
+++ lib/discord_async/resources/application/resolved_data.rb
@@ -1,17 +1,14 @@
-
-
-
-module DiscordAsync
-  module Resources
-    class Interaction < StructBase
-      class ResolvedData < StructBase
-        attribute? :users,
-        attribute? :members,
-        attribute? :roles,
-        attribute? :channels,
-        attribute? :messages,
-        attribute? :attachments,
-      end
-    end
-  end
-end
+# module DiscordAsync
+#   module Resources
+#     class Interaction < StructBase
+#       class ResolvedData < StructBase
+#         attribute? :users,
+#         attribute? :members,
+#         attribute? :roles,
+#         attribute? :channels,
+#         attribute? :messages,
+#         attribute? :attachments,
+#       end
+#     end
+#   end
+# end
