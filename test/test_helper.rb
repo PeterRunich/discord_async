c3450dd96722468a88668f675a4837879e644ad5
Peter Runich <p.runich@bk.ru>
2023-02-20 16:55:04 +0300

    simplecov added useless test gems removed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,12 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'discord_async'
+require 'debug'
+
+require 'simplecov'
+SimpleCov.start
 
 require 'minitest/autorun'
-require 'minitest/pride'
 require 'color_pound_spec_reporter'
-require 'minitest/stub_any_instance'
-require 'spy/integration'
-require_relative 'boolean_assertion'
 
 Minitest::Reporters.use! [ColorPoundSpecReporter.new] unless ENV['RM_INFO']
